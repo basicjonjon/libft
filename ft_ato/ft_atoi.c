3d54d2bdc620b8582f13d3d513354fa18d5ae972
Jonatan Le Douaron <jle-doua@made-f0Ar8s5.clusters.42paris.fr>
2024-09-11 14:20:01 +0200

    libft

---
+++ ft_ato/ft_atoi.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jle-doua <jle-doua@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/20 10:20:56 by jle-doua          #+#    #+#             */
+/*   Updated: 2024/09/10 15:17:32 by jle-doua         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	count;
+	int	res;
+
+	i = 0;
+	count = 0;
+	res = 0;
+	while (ft_isspace(str[i]))
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+		if (str[i++] == '-')
+			count++;
+	while (str[i] >= '0' && str[i] <= '9')
+		res = res * 10 + (str[i++] - '0');
+	if (count % 2)
+		res *= -1;
+	return (res);
+}
+
+
---
+++ ft_ato/ft_atol.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jle-doua <jle-doua@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 15:17:13 by jle-doua          #+#    #+#             */
+/*   Updated: 2024/09/10 15:24:13 by jle-doua         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/libft.h"
+
+long	ft_atol(const char *str)
+{
+	int i;
+	int count;
+	long res;
+
+	i = 0;
+	count = 0;
+	res = 0;
+	while (ft_isspace(str[i]))
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+		if (str[i++] == '-')
+			count++;
+	while (str[i] >= '0' && str[i] <= '9')
+		res = res * 10 + (str[i++] - '0');
+	if (count % 2)
+		res *= -1;
+	return (res);
+}
